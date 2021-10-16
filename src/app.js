2fac0cd5545958e003b38df75a2285db20e3c089
Dylan <dflauzon@gmail.com>
2021-10-16 11:10:24 -0400

    Set up handlebar, learned to use partials and views. Basic styling of page.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,43 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname,'..','/public')
 
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname,'..','/public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Dylan Lauzon'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Dylan Lauzon'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        msg: 'Get help at github.com/dfrl-dev/web-server',
+        name: 'Dylan Lauzon'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send([{
@@ -15,6 +48,22 @@ app.get('/weather', (req, res) => {
         location: 'Timmins',
         forecast: 'Gloomy'
     }])
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Dylan Lauzon',
+        errorMessage: 'Help article not found'
+    })
+})
+
+app.get('*', (req,res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Dylan Lauzon',
+        errorMessage: 'Page not found'
+    })
 })
 
 app.listen(3000, () => {
